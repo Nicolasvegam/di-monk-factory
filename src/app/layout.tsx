29b3fecaa15e88e360256fa6ad14645cf5606f2a
Nicolasvegam <ndvega@uc.cl>
2025-02-03 21:15:47 -0300

    fix(layout)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,7 +97,7 @@ export default function RootLayout({
                 },
                 "offers": {
                   "@type": "Offer",
-                  "price": "Consultar",
+                  "price": "990",
                   "priceCurrency": "USD",
                   "availability": "https://schema.org/InStock"
                 }
